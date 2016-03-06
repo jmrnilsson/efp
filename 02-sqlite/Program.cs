4e721d9cec960707b0d18b7e659fdbe5bf00d0fe
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-06 13:26:49 +0100

    add sqlite

---
+++ 02-sqlite/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello EF!");
+        }
+    }
+}
