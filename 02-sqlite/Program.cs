b4e3c1fe1fa9a092181dbd8553efabfe207e66a6
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-06 17:12:18 +0100

    add model and program

--- 02-sqlite/Program.cs
+++ 02-sqlite/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleApp
     {
         public static void Main()
         {
-            Console.WriteLine("Hello EF!");
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(String.Join(", ", blog.Url, blog.BlogId, blog.Name, blog.Posts));
+                }
+            }
         }
     }
 }
