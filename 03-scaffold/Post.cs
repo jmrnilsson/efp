779bb5b2b8ab0b357350dab88edefc119d849e20
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-08 19:31:54 +0100

    successfull true scaffold from empty folder

---
+++ 03-scaffold/Post.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace 03-scaffold
+{
+    public partial class Post
+    {
+        public long PostId { get; set; }
+        public long BlogId { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+
+        public virtual Blog Blog { get; set; }
+    }
+}
