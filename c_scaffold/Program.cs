1fbf35c3aa87a4f65e983b412e2cb805256c20d4
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-08 22:14:22 +0100

    add stop watch to Program

--- c_scaffold/Program.cs
+++ c_scaffold/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.Data.Entity;
 
@@ -11,23 +12,29 @@ namespace c_scaffold
         {
             using (var db = new blogContext())
             {
-                var start = DateTime.UtcNow;
-                var items =
-                (
-                    from p in db.Post
-                    select new {p.Blog.Url, p.Title, p.Content}
-                ).ToList();
-                var duration = DateTime.UtcNow - start;
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                var expression =
+                    from b in db.Blog
+                    join p in db.Post on b.BlogId equals p.BlogId
+                    select new {b.Url, p.Title, p.Content};
 
-                foreach(var item in items)
+                long interpret = stopWatch.ElapsedMilliseconds;
+                var items = expression.ToList();
+                long read = stopWatch.ElapsedMilliseconds - interpret;
+                var filtered = items.Where(i => i.Url == "768aea71-c405-4808-b1e6-150692142875").ToList();
+                long scan = stopWatch.ElapsedMilliseconds - read;
+
+                foreach(var item in filtered)
                 {
                     Console.WriteLine("{0} - {1} - {2}", item.Url, item.Title, item.Content);
                 }
 
                 Console.WriteLine();
-                Console.WriteLine("{0} records read in {1} ms ", items.Count(), duration.TotalMilliseconds);
-                Console.WriteLine("Total of {0} ms per record", duration.TotalMilliseconds / items.Count());
-
+                Console.WriteLine("{0} records read in {1} ms", items.Count(), read);
+                Console.WriteLine("Average {0} ms per record", Math.Round((double) read / items.Count(), 2));
+                Console.WriteLine("Expression interpretation in {0} ms", interpret);
+                Console.WriteLine("In-memory scan in {0} ms", scan);
             }
         }
     }
