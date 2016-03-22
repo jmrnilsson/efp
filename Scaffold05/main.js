920b2975a11d0cf6f615717e9a177e624a489135
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-23 00:09:04 +0100

    update electron app

--- Scaffold05/main.js
+++ Scaffold05/main.js
@@ -13,7 +13,7 @@ let mainWindow;
 function createWindow () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
