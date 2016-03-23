14dfccd985a4dca03007b09122ad491e5ff26278
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-23 23:43:16 +0100

    update with ko

--- Scaffold06/view.js
+++ Scaffold06/view.js
@@ -1,6 +1,9 @@
 'use strict';
 const ipcRenderer = require('electron').ipcRenderer;
+const fs = require('fs');
+const ko = require('./curl/knockout-3.4.0.js');
 
+/*
 var textarea_value;
 
 var button = document.getElementsByTagName('input')[0];
@@ -11,7 +14,7 @@ button.onclick = function(){
     let expression = textarea.innerHTML;
     ipcRenderer.send('my-msg', expression);
 }
-
+*/
 ipcRenderer.on('asynchronous-reply', function(event, arg) {
   console.log(arg);
 });
@@ -19,3 +22,12 @@ ipcRenderer.on('asynchronous-reply', function(event, arg) {
 function callback(sender, arg) {
     textarea_value = arg;
 }
+
+function QueryModel() {
+    var self = this;
+    self.csscript = ko.observable(fs.readFileSync('./Program.cs.001'));
+    self.go = function () {
+        ipcRenderer.send('my-msg', self.csscript());
+    }
+}
+ko.applyBindings(new QueryModel(), document.getElementsByTagName('body')[0]);
