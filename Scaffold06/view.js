3492d6abc8bee549aaecac8ebe485d35971cb443
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-25 00:04:27 +0100

    added proper support for codemirror (including removal of code inline scroll). add menus and buttons. added font icons (using curl again).

--- Scaffold06/view.js
+++ Scaffold06/view.js
@@ -4,16 +4,21 @@ const ipc = require('electron').ipcRenderer;
 const fs = require('fs');
 const ko = require('./curl/knockout-3.4.0.js');
 const Q = require('q');
+const CodeMirror = require('codemirror');
 
-// var csscript = ko.observable('');
-var go = () => { ipc.send('go', csscript()); };
+function runScript() {
+    ipc.send('go', editor.getValue());
+}
 
-var editorOptions = { lineNumbers: true, mode: 'csharp' };
+var editorOptions = {
+    lineNumbers: true,
+    mode: 'clike'
+};
+
 var textArea = document.getElementsByTagName('textarea')[0];
 var editor = CodeMirror.fromTextArea(textArea, editorOptions);
 
 Q.nfbind(fs.readFile)('./dotnet/Program.cs.003', 'utf8').then(e => {
-    // csscript(e)
     editor.setValue(e);
     editor.refresh();
 });
@@ -22,11 +27,7 @@ ipc.on('on-result', function(event, arg) {
   console.log(arg);
 });
 
-function QueryModel() {
-    var self = this;
-    self.go = go
-    // self.csscript = csscript;
-}
-
-ko.applyBindings(new QueryModel(), document.getElementsByTagName('body')[0]);
+var el = document.getElementsByTagName('body')[0];
+var model = {runScript: runScript};
+ko.applyBindings(model, el);
 
