cd97688c60990bdee5c7db95ca50e320d89cc0b8
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-25 03:42:15 +0100

    fixed lintstuff for es6 and added rc for eslint

--- Scaffold06/view.js
+++ Scaffold06/view.js
@@ -5,36 +5,34 @@ const fs = require('fs');
 const ko = require('./curl/knockout-3.4.0.js');
 const Q = require('q');
 const CodeMirror = require('codemirror');
-const smooths = require('./scroll.js')
+const smooths = require('./scroll.js');
 
-let headers = ko.observableArray();
-let rows = ko.observableArray();
+const headers = ko.observableArray();
+const rows = ko.observableArray();
+const textArea = document.getElementsByTagName('textarea')[0];
+const editorOptions = {
+    lineNumbers: true,
+    mode: 'clike',
+};
+const editor = CodeMirror.fromTextArea(textArea, editorOptions);
 
 function run() {
     ipc.send('go', editor.getValue());
 }
 
-var editorOptions = {
-    lineNumbers: true,
-    mode: 'clike'
-};
-
-var textArea = document.getElementsByTagName('textarea')[0];
-var editor = CodeMirror.fromTextArea(textArea, editorOptions);
 
 Q.nfbind(fs.readFile)('./dotnet/Program.cs.003', 'utf8').then(e => {
     editor.setValue(e);
     editor.refresh();
 });
 
-ipc.on('on-result', function(event, args) {
-    console.log(args);
+ipc.on('on-result', (event, args) => {
     let index = 0;
-    let dbHeaders = {};
-    let dbRows = [];
-    for (var i = 0; i < args.length; i++) {
-        var row = [];
-        for(var attr in args[i]) {
+    const dbHeaders = {};
+    const dbRows = [];
+    for (let i = 0; i < args.length; i++) {
+        const row = [];
+        for (let attr in args[i]) {
             if (Object.prototype.hasOwnProperty.call(args[i], attr)) {
                 if (!Object.prototype.hasOwnProperty.call(dbHeaders, attr)) {
                     dbHeaders[attr] = index++;
@@ -47,9 +45,9 @@ ipc.on('on-result', function(event, args) {
     rows.removeAll();
     headers.removeAll();
     dbRows.forEach(r => rows.push(r));
-    var len = Object.getOwnPropertyNames(dbHeaders).length;
-    for (var i = 0; i < len; i++) {
-        for(var attr in dbHeaders) {
+    const len = Object.getOwnPropertyNames(dbHeaders).length;
+    for (let i = 0; i < len; i++) {
+        for (let attr in dbHeaders) {
             if (Object.prototype.hasOwnProperty.call(dbHeaders, attr)) {
                 if (dbHeaders[attr] === i) {
                     headers.push(attr);
@@ -59,7 +57,7 @@ ipc.on('on-result', function(event, args) {
     }
 });
 
-var el = document.getElementsByTagName('body')[0];
-var model = {run: run, headers: headers, rows: rows, smoothScroll: smooths.smoothScroll};
+const el = document.getElementsByTagName('body')[0];
+const model = {run, headers, rows, smoothScroll: smooths.smoothScroll};
 ko.applyBindings(model, el);
 
