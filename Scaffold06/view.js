550e070c6cbe40001f19a60d6e4020761d9552c8
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-03-25 01:05:37 +0100

    early updates for table (headers and rows)

--- Scaffold06/view.js
+++ Scaffold06/view.js
@@ -6,7 +6,10 @@ const ko = require('./curl/knockout-3.4.0.js');
 const Q = require('q');
 const CodeMirror = require('codemirror');
 
-function runScript() {
+let headers = ko.observableArray();
+let rows = ko.observableArray();
+
+function run() {
     ipc.send('go', editor.getValue());
 }
 
@@ -23,11 +26,38 @@ Q.nfbind(fs.readFile)('./dotnet/Program.cs.003', 'utf8').then(e => {
     editor.refresh();
 });
 
-ipc.on('on-result', function(event, arg) {
-  console.log(arg);
+ipc.on('on-result', function(event, args) {
+    console.log(args);
+    let index = -1;
+    let dbHeaders = {};
+    let dbRows = [];
+    for (var i = 0; i < args; i++) {
+        for(var attr in args[i]) {
+            if (args.hasOwnProperty(attr)) {
+                if (!headers.hasOwnProperty(attr)) {
+                    headers[attr] = index++;
+                }
+                var row = [];
+                row[headers[attr]] = args[i][attr];
+                dbRows.push(row);
+            }
+        }
+    }
+    rows.removeAll();
+    headers.removeAll();
+    dbRows.forEach(r => rows.push(r));
+    for (var i = 0; i < dbHeaders.length; i++) {
+        for(var attr in dbHeaders) {
+            if (arg.hasOwnProperty(attr)) {
+                if (attr === i) {
+                    headers.push(dbHeaders[attr]);
+                }
+            }
+        }
+    }
 });
 
 var el = document.getElementsByTagName('body')[0];
-var model = {runScript: runScript};
+var model = {run: run, headers: headers, rows: rows};
 ko.applyBindings(model, el);
 
